bcff077a1e3eb4fa50ebf7cb188a72a0d254a3b0
sahanlakmal13 <sahanlakmal13@gmail.com>
2023-03-10 20:58:57 +0530

    removed debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -9,8 +9,6 @@ import 'package:pollard/screens/welcome_screen.dart';
 import 'screens/input_page.dart';
 import 'screens/dataset.dart';
 
-
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
@@ -20,24 +18,24 @@ void main() async {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
-        theme: ThemeData.dark().copyWith(
-          primaryColor: Color(0xFF0A0E21),
-          scaffoldBackgroundColor: Color(0xFF0A0E21), colorScheme: ColorScheme.fromSwatch().copyWith(secondary: Colors.purple),
-        ), 
-        initialRoute: WelcomeScreen.id,
-        routes: {
-          MultiplicationScreen.id:(context) => MultiplicationScreen(),
-          WelcomeScreen.id:(context) => WelcomeScreen(),
-          LoginScreen.id:(context) => LoginScreen(),
-          RegistrationScreen.id:(context) => RegistrationScreen(),
-          MyStudents.id:(context) => MyStudents(),
-          Home_Screen.id:(context) => Home_Screen(),
-          Sales_Screen.id:(context) => Sales_Screen(), 
-
-        },
-      );
-    
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(
+        primaryColor: Color(0xFF0A0E21),
+        scaffoldBackgroundColor: Color(0xFF0A0E21),
+        colorScheme:
+            ColorScheme.fromSwatch().copyWith(secondary: Colors.purple),
+      ),
+      initialRoute: WelcomeScreen.id,
+      routes: {
+        MultiplicationScreen.id: (context) => MultiplicationScreen(),
+        WelcomeScreen.id: (context) => WelcomeScreen(),
+        LoginScreen.id: (context) => LoginScreen(),
+        RegistrationScreen.id: (context) => RegistrationScreen(),
+        MyStudents.id: (context) => MyStudents(),
+        Home_Screen.id: (context) => Home_Screen(),
+        Sales_Screen.id: (context) => Sales_Screen(),
+      },
+    );
   }
 }
-
